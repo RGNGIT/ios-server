0ee8a9e4f2df25a59f060cf86f83b8750d85df58
Nick <68756004+RGNGIT@users.noreply.github.com>
2022-03-17 22:45:24 +0500

    Update api-check.ts

--- server/src/middlewares/api-check.ts
+++ server/src/middlewares/api-check.ts
@@ -1,5 +1,5 @@
 export function apiCheck(req, res, next) {
-    if (req.originalUrl.includes('/api')) {
+    if (req.originalUrl.includes(process.env.API_CALL)) {
         next();
     } else {
         res.sendFile('index.html', {root: '../front/main-page'});
