e1e30f779f8de49e65228da529ce55ee2f8a1c28
RGNGIT <vnifedow@bk.ru>
2023-04-12 19:26:34 +0500

    Patch

--- server/const/respond.ts
+++ server/const/respond.ts
@@ -34,7 +34,8 @@ enum ErrCode {
   SUBMIT_TEST_RESULT_ERROR =       32,
   GET_TEST_RESULTS_ERROR =         33,
   GET_COMPLETE_DISCIPLINES_ERROR = 34,
-  GET_ALL_USERS_ERROR =            35
+  GET_ALL_USERS_ERROR =            35,
+  GET_STORED_STATUS_ERROR =        36
 }
 
 class ResultHandler {
